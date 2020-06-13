d0ee6184706a8f37f068b2460422c16abee2f964
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-06-13 14:48:14 +0430

    Initial Commit

---
+++ Fajr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fajr
+//
+//  Created by Seyed Ali Shahrokhi on 3/24/1399 AP.
+//  Copyright © 1399 Seyed Ali Shahrokhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
